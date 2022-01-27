181d4625335a1f79cbbaf8e4a3e204e404585384
caihe <2462389854@qq.com>
2022-01-28 00:05:19 +0800

    🖍 中间件逻辑修改

--- src/middleWare/auth.js
+++ src/middleWare/auth.js
@@ -1,15 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2022-01-27 17:42:20
- * @LastEditTime: 2022-01-27 18:15:03
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-01-27 23:59:39
+ * @LastEditors: your name
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \Coders\src\middleWare\auth.js
+ * @FilePath: \iNote_BE\src\middleWare\auth.js
  */
 module.exports = options => {
-  const inspirecloud = require("@byteinspire/api");
+  const inspirecloud = require('@byteinspire/inspirecloud-api');
   return async(req, res, next) => {
-    const user = await inspirecloud.user.current(req);
+    const {headers} = req;
+    const user = await inspirecloud.user.current({headers});
     if (!user) {
       res.status(401);
       res.send({
