ff0bd7328ea89ba8d1d46674122926e9256eaf53
caihe <2462389854@qq.com>
2022-02-09 21:47:30 +0800

    📧 邮件文案修改

--- src/utils/pushNotice.js
+++ src/utils/pushNotice.js
@@ -1,7 +1,7 @@
 /*
  * @Author: chichiksky
  * @Date: 2022-02-08 21:54:20
- * @LastEditTime: 2022-02-09 17:18:34
+ * @LastEditTime: 2022-02-09 21:45:09
  * @LastEditors: your name
  * @Description:
  * @FilePath: \iNote_BE\src\utils\pushNotice.js
@@ -15,17 +15,19 @@ const ObjectId = inspirecloud.db.ObjectId;
 const sendEmailFromQQ = require("./sendEmailFromQQ");
 // 推送笔记的hook方法
 module.exports = async function (note) {
+  const items = [
+    "任何时候做任何事，订最好的计划，尽最大的努力，作最坏的准备。",
+    "你因成功而内心充满喜悦的时候，就没有时间颓废。",
+    "最重要的就是不要去看远方模糊的，而要做手边清楚的事。",
+    "吾观自古贤达人，成功不退皆殒身。",
+    "理想是力量的泉源、智慧的摇篮、冲锋的战旗、斩棘的利剑。",
+    "自己打败自己是最可悲的失败，自己战胜自己是最可贵的胜利。",
+    "人所缺乏的不是才干而是志向，不是成功的能力而是勤劳的意志。"
+  ]
   // 找到对应的用户
   const user = await userTable.where({ _id: ObjectId(note.author) }).findOne();
-  var text = `------
-  笔记推送：
-  _id: ${note._id}
-  title: ${note.title}
-  author: ${note.author}
-  pushTime: ${note.schedule[note.round]}
-  pushRound: ${note.round}
-  realPushTime: ${dayjs().format()}
-  ------`;
+  var text = `Hello!！iNote提醒您,又到复习时间了,这是您笔记《${note.title}》的第${note.round}次推送,请及时登录网站复习哦! 
+      ————${items[note.round]}`
   // 调用QQ邮箱推送内容
   // 需要用户填写邮箱
   await sendEmailFromQQ(user.email, "推送成功!", text);
