f5789a94482786a00e901c9350078c7894afa361
Xav1erSue <s791521574@live.com>
2022-02-10 00:22:40 +0800

    debug: 部署调试

--- src/utils/polling.js
+++ src/utils/polling.js
@@ -1,23 +1,23 @@
 const dayjs = require("dayjs");
-const isBetween = require("dayjs/plugin/isBetween");
-dayjs.extend(isBetween);
+const isSameOrAfter = require("dayjs/plugin/isSameOrAfter");
+dayjs.extend(isSameOrAfter);
 const noteTable = require("../models/noteTable");
 const pushNotice = require("../utils/pushNotice");
 
 module.exports = function () {
-  // 每半分钟对笔记数据表做轮询
+  // 对笔记数据表做轮询
   setInterval(async () => {
     const now = dayjs();
-    const start = now.startOf("minute");
-    const end = now.endOf("minute");
     const noteList = await noteTable.where({ needPush: true }).find();
-    // 筛选出在这一分钟内达到推送时间的笔记
-    const pushList = noteList.filter((el) =>
-      dayjs(el.schedule[el.round]).isBetween(start, end)
+    // 筛选出已经达到推送时间的笔记
+    const pushList = noteList.filter((note) =>
+      now.isSameOrAfter(dayjs(note.schedule[note.round]))
     );
+    // 日志
+    console.log(`${dayjs().format("M月D日 HH:mm:ss")} pushList:${pushList}`);
     // 推送hook，对每一个待推送笔记调用推送方法
-    pushList.forEach((el) => {
-      pushNotice(el);
+    pushList.forEach(async (note) => {
+      await pushNotice(note);
     });
-  }, 30000);
+  }, 60000);
 };
